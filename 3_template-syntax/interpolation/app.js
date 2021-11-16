fe00cfdf6d2ad37d69d19ebc728077119e4d6ffd
reyhan-farabi <reyhanfarabi@refactory.id>
2021-11-16 19:30:29 +0700

    3_template-syntax interpolation

---
+++ 3_template-syntax/interpolation/app.js
@@ -0,0 +1,24 @@
+// App 1
+var app = new Vue({
+  el: "#app-1",
+  data: {
+    msg: "Hello World",
+  },
+});
+
+// App 2
+var app2 = new Vue({
+  el: "#app-2",
+  data: {
+    msg: "Test Message",
+  },
+});
+
+// App 3
+var app3 = new Vue({
+  el: "#app-3",
+  data: {
+    msg: "Hello World",
+    number: 5,
+  },
+});
