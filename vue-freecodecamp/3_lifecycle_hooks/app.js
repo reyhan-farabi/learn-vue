73231b1a40c58aaa8b2fef264f61e6182f852ecf
reyhan-farabi <reyhanfarabi@refactory.id>
2021-11-17 20:22:00 +0700

    testing multiple lifecycle hooks

--- vue-freecodecamp/3_lifecycle_hooks/app.js
+++ vue-freecodecamp/3_lifecycle_hooks/app.js
@@ -9,5 +9,25 @@ let app = Vue.createApp({
       this.isVisible = !this.isVisible;
     },
   },
+  updated() {
+    console.log("updated");
+  },
 });
+
+app.component("test-box", {
+  template: `
+    <div class='box'></div>
+  `,
+  created() {
+    console.log("created");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  unmounted() {
+    console.log("unmounted");
+    console.log("unmounted");
+  },
+});
+
 app.mount("#app");
