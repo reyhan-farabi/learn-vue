e99a4b355e0b1c744545467aa1ed45a0830335a8
reyhan-farabi <reyhanfarabi@refactory.id>
2021-11-17 17:44:19 +0700

    added custom-input components

--- vue-freecodecamp/2_components/app.js
+++ vue-freecodecamp/2_components/app.js
@@ -15,28 +15,45 @@ let app = Vue.createApp({
   },
 });
 
-// custom-form
+// login-form components
 app.component("login-form", {
   template: `
     <form @submit.prevent='handleSubmit'>
       <h1>{{title}}</h1>
 
-      <input type='email'/>
-      <input type='password'/>
+      <custom-input type='email' v-bind:label='emailLabel'/>
+      <custom-input type='password' v-bind:label='passwordLabel'/>
 
       <button>Log in</button>
     </form>
   `,
+  components: ["custom-input"],
   data() {
     return {
       title: "Login Form",
+      email: "",
+      password: "",
+      emailLabel: "Email",
+      passwordLabel: "Password",
     };
   },
   methods: {
     handleSubmit() {
-      console.log("submitted");
+      console.log(this.email, this.password);
     },
   },
 });
 
+// custom-input components
+app.component("custom-input", {
+  template: `
+    <label>
+      {{label}}
+      <input type='text'/>
+    </label>
+  `,
+  props: ["label"],
+});
+
+// mount app
 app.mount("#app");
