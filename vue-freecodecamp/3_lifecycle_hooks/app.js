5d5405f2e58b75620766455110b53b24134aaf44
reyhan-farabi <reyhanfarabi@refactory.id>
2021-11-17 20:11:38 +0700

    modified code

--- vue-freecodecamp/3_lifecycle_hooks/app.js
+++ vue-freecodecamp/3_lifecycle_hooks/app.js
@@ -1,16 +1,12 @@
 let app = Vue.createApp({
   data: function () {
     return {
-      greeting: "Hello using Vue 3",
       isVisible: false, // try to change this to true
     };
   },
   methods: {
     toggleBox() {
       this.isVisible = !this.isVisible;
-    },
-    greet(greeting) {
-      console.log(greeting);
     },
   },
 });
