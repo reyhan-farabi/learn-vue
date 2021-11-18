aa6aaa0037c1d88a7bd312f7d40e5798a3d28959
reyhan-farabi <reyhanfarabi@refactory.id>
2021-11-18 16:33:55 +0700

    added methods

--- baledemy-vue/1_basic/app.js
+++ baledemy-vue/1_basic/app.js
@@ -1,9 +1,15 @@
 const member = {
   title: "Vue JS",
   subtitle: "This is my test comment",
+  name: "Reyhan",
 };
 
 const vm = new Vue({
   el: "#app",
   data: member,
+  methods: {
+    getName() {
+      return "Hello, " + this.name;
+    },
+  },
 });
