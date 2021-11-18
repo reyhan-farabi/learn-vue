871ad9cca7e0d5b754b6441c6d9d67b65c139889
reyhan-farabi <reyhanfarabi@refactory.id>
2021-11-18 21:36:43 +0700

    created a few components

--- baledemy-vue/2_components/app.js
+++ baledemy-vue/2_components/app.js
@@ -1,3 +1,33 @@
+Vue.component("header-component", {
+  template: `
+    <header>
+        <h1>Welcome to {{ framework }}</h1>
+        <h3>Hi, {{ firstname }} {{ lastname }}</h3>
+    </header>
+    `,
+  data() {
+    return {
+      framework: "Vue JS",
+      firstname: "Reyhan",
+      lastname: "Farabi",
+    };
+  },
+});
+
+Vue.component("footer-component", {
+  template: `
+    <footer id="footer">
+        <p>Copyright 2021 - M. Reyhan Farabi</p>
+    </footer>
+    `,
+});
+
+Vue.component("my-list", {
+  template: `
+        <li>Test</li>
+    `,
+});
+
 const member = {
   title: "Vue JS",
   subtitle: "This is my test comment",
